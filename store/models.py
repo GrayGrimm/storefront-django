28240ab5b6540e63480551a481e416b3b26226b8
guyera <thegraygrimm@graymansurvivaltech.com>
2025-11-12 15:59:24 -0500

    Connected SQL to DJANGO

--- store/models.py
+++ store/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = "B"
     MEMBERSHIP_SILVER = "S"
-    MEMBERSHIP_GOLD = "D"
+    MEMBERSHIP_GOLD = "G"
 
     MEMBERSHIP_CHOICES = [
         (MEMBERSHIP_BRONZE, "Bronze"),
